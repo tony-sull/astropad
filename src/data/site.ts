0ac7e418e20b941f21320f6cd42b9d3b79a36d97
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-03 15:49:41 -0500

    Adding a footer, cleaning up more styles

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,10 +1,12 @@
 export interface Site {
     name: string
+    footer: string
     githubUrl?: string
 }
 
 const site: Site = {
     name: 'Astropad',
+    footer: 'Made with ❤ by Astro',
     githubUrl: 'https://github.com/tony-sull/astropad'
 }
 
