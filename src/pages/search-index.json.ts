f358a43fd3c6794a943c4025d34dfc8b85a96192
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-04 22:22:38 -0500

    lint fixes

--- src/pages/search-index.json.ts
+++ src/pages/search-index.json.ts
@@ -1,26 +1,26 @@
 import elasticlunr from 'elasticlunr'
-import type { APIRoute, MDXInstance } from 'astro'
+import type { APIRoute } from 'astro'
 import type { Props as Page } from '../layouts/Page.astro'
 
 export const get: APIRoute = () => {
-    const index = elasticlunr(function() {
-        this.addField('title')
-        this.addField('description')
-        this.addField('id')
+  const index = elasticlunr(function () {
+    this.addField('title')
+    this.addField('description')
+    this.addField('id')
+  })
+
+  const allPages = import.meta.glob<Page>('./**/*.mdx', { eager: true })
+
+  for (const { frontmatter, url } of Object.values(allPages)) {
+    index.addDoc({
+      id: url,
+      title: frontmatter.title,
+      description: frontmatter.description,
+      // content: TODO
     })
+  }
 
-    const allPages = import.meta.glob<Page>('./**/*.mdx', { eager: true })
-
-    for (const { frontmatter, url } of Object.values(allPages)) {
-        index.addDoc({
-            id: url,
-            title: frontmatter.title,
-            description: frontmatter.description,
-            // content: TODO
-        })
-    }
-
-    return {
-        body: JSON.stringify(index.toJSON())
-    }
+  return {
+    body: JSON.stringify(index.toJSON()),
+  }
 }
