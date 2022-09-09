bcf61504cc9c8d1f95e18318f54b96d1f0cf5a96
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-08 21:53:52 -0500

    Adding breadcrumb support

---
+++ src/utils/navigation.ts
@@ -0,0 +1,95 @@
+import type { MDXInstance } from 'astro'
+import { DepGraph } from 'dependency-graph'
+import type { SEO } from '../data/seo.js'
+
+export interface Page extends SEO {
+  navigation?: {
+    order: number
+  }
+}
+
+export interface Entry {
+  title: string
+  url: string
+  order: number
+  parentKey?: string
+  children?: Entry[]
+}
+
+function getParentKey(url: string) {
+  const segments = url.split('/')
+
+  if (segments.length === 1) return undefined
+  return segments.slice(0, segments.length - 1).join('/')
+}
+
+export function findNavigationEntries(nodes: MDXInstance<Page>[] = [], key = '') {
+  let pages: Entry[] = []
+
+  for (const entry of nodes) {
+    if (entry.frontmatter.navigation) {
+      const nav = entry.frontmatter.navigation
+      const parent = getParentKey(entry.url!)
+      if ((!key && !parent) || parent === key) {
+        pages.push({
+          ...nav,
+          title: entry.frontmatter.title,
+          url: entry.url!,
+        })
+      }
+    }
+  }
+
+  return pages
+    .sort((a, b) => a.order - b.order)
+    .map((entry) => {
+      if (entry.url) {
+        entry.children = findNavigationEntries(nodes, entry.url)
+      }
+
+      return entry
+    })
+}
+
+function findDependencies(pages: Entry[], depGraph: DepGraph<Entry>, parentKey?: string) {
+  for (let page of pages) {
+    depGraph.addNode(page.url, page)
+    if (parentKey) {
+      depGraph.addDependency(page.url, parentKey)
+    }
+    if (page.children) {
+      findDependencies(page.children, depGraph, page.url)
+    }
+  }
+}
+
+function getDependencyGraph(nodes: MDXInstance<Page>[] = []) {
+  let pages = findNavigationEntries(nodes)
+  let graph = new DepGraph<Entry>()
+  findDependencies(pages, graph)
+  return graph
+}
+
+export function findBreadcrumbEntries(
+  nodes: MDXInstance<Page>[],
+  activeKey: string,
+  options: { includeSelf: boolean } = { includeSelf: true }
+) {
+  let graph = getDependencyGraph(nodes)
+  if (!graph.hasNode(activeKey)) {
+    // Fail gracefully if the key isn't in the graph
+    return []
+  }
+  let deps = graph.dependenciesOf(activeKey)
+  if (options.includeSelf) {
+    deps.push(activeKey)
+  }
+
+  return activeKey
+    ? deps.map((key) => {
+        let data = Object.assign({}, graph.getNodeData(key))
+        delete data.children
+        return data
+      })
+    : []
+}
